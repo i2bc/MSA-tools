17848f0c029aa011feab662cc0bd02e70d63727d
Chloe Quignot <chloe.quignot@cea.fr>
2024-07-16 11:23:38 +0200

    fix(msa-tools): fix pdb2cif by removing score section

--- pdb2cif.py
+++ pdb2cif.py
@@ -42,58 +42,6 @@ def convert2cif(pdb_file,cif_file):
     io = MMCIFIO()
     io.set_structure(struc)
     io.save(cif_file)
-
-    str_scores = """#
-loop_
-_ma_qa_metric.id
-_ma_qa_metric.mode
-_ma_qa_metric.name
-_ma_qa_metric.software_group_id
-_ma_qa_metric.type
-1 global pLDDT 1 pLDDT 
-2 local  pLDDT 1 pLDDT 
-#
-_ma_qa_metric_global.metric_id    1
-_ma_qa_metric_global.metric_value {:2.2f}
-_ma_qa_metric_global.model_id     1
-_ma_qa_metric_global.ordinal_id   1
-# 
-loop_
-_ma_qa_metric_local.label_asym_id
-_ma_qa_metric_local.label_comp_id
-_ma_qa_metric_local.label_seq_id
-_ma_qa_metric_local.metric_id
-_ma_qa_metric_local.metric_value
-_ma_qa_metric_local.model_id
-_ma_qa_metric_local.ordinal_id
-"""
-    
-    regex = "ATOM[\s]*[\d]+[\s]*[A-Z] [A-Z0-9]+[\s]*[\.A-Z0-9][\s]*([A-Z]+)[\s]*([\S]+)[\s]*([\S]+)[\s]*([\d]+)[\s]*[\S]+[\s]*[\-\d\.]+[\s]*[\-\d\.]+[\s]*[\-\d\.]+[\s]*[\-\d\.]+[\s]*([\-\d\.]+)[\s]*[\-\d\.]+[\s]*[\S]+[\s]*[\d]+"
-    prev_res = ("0","0","0","0","0")
-    global_score = 0
-    total = 0
-    with open(cif_file) as f:
-        for line in f:
-            if line.startswith("ATOM"):
-                res = re.search(regex,line)
-                if not res:
-                    os.remove(cif_file)
-                    print("\nError: something went wrong with the regex...\n")
-                    print(pdb_file)
-                    print(line)
-                    print(regex)
-                    return()
-                aa, ch, entity, resnum, bfact = res.groups()
-                if " ".join((aa, ch, entity, resnum, bfact)) != " ".join(prev_res):
-                    str_scores += f"{ch} {aa} {resnum: <3} 2 {bfact: <5} 1 {resnum: <3} \n"
-                prev_res = (aa, ch, entity, resnum, bfact)
-                global_score += float(bfact)
-                total += 1
-    str_scores+="#\n"
-
-    with open(cif_file, "a") as f:
-        f.write(str_scores.format(global_score*1./total))
-    
     print("pdb2cif done!")
 
 
