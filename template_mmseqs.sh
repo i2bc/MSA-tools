786e12e8dc334d6cc2f70d30e381151757ff5f3d
Hugo <hugo.pointier@i2bc.paris-saclay.fr>
2024-07-19 15:37:59 +0200

    clean up of temporary files in script

--- template_mmseqs.sh
+++ template_mmseqs.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+
 #PBS -N colabsearch
 #PBS -q alphafold
 #PBS -o $LOGDIR/colabsearch.output.log
@@ -14,20 +15,23 @@
 # PROGRESSLOG => name and path to the job progress log file (for printing on the webserver)
 # INPUT => name and path to the fasta input file
 
-
 set -x
-
 echo Host $(hostname)
 echo Jobid $PBS_JOBID
+
 #======= USER COMMANDS =======
+
 echo Start job: $(date)
 mkdir -p $OUTDIR
 cd $OUTDIR
+
 # write log to file
 echo "[$(date '+%F %T')] Loading database..." >> $PROGRESSLOG
 DB=/database/db/uniref30_2202/uniref30_2202_db.idx
+
 # check for previous DBs
 ISVM=$(ps aux | grep vm | grep -v grep | wc -l)
+
 if [[ $ISVM -eq 1 ]]; then
     # a DB is loaded, check if it's the right one
     ISDB=$(ps aux | grep $DB | grep -v grep | wc -l)
@@ -37,8 +41,10 @@ if [[ $ISVM -eq 1 ]]; then
         sudo /usr/local/bin/mmseqs_unload -f $OLDDB
     fi
 fi
+
 # load the DB
 sudo /usr/local/bin/mmseqs_load -f $DB
+
 # write log to file
 echo "[$(date '+%F %T')] Running step 1 - colabfold_search msa generation..." >> $PROGRESSLOG
 
@@ -47,6 +53,7 @@ singularity exec --containall --env TMPDIR=$TMPDIR -B $PWD,/store,/data,/opt/alp
 
 # check for colabfold_search output
 myarray=(`find $OUTDIR -maxdepth 1 -name "*.a3m"`)
+
 if [ ${#myarray[@]} -eq 0 ]; then 
     echo "Error: colabfold_search (MMseqs2) step ended with an error (mmseqs outputs weren't generated in $(basename $OUTDIR))" >> $PROGRESSLOG
     exit 1
@@ -58,8 +65,10 @@ echo "[$(date '+%F %T')] ...output ok" >> $PROGRESSLOG
 # rename the output a3m after colabfold_search
 cd $OUTDIR
 echo "[$(date '+%F %T')] ...renaming MSA output files" >> $PROGRESSLOG
+
 for file in $(ls *.a3m); do
     filename=$(python3 <<EOF
+
 import re
 input = open("${file}", 'r')
 while input:
@@ -72,10 +81,12 @@ EOF
 )
 
     # if output name already exists, we add a number at the end
+
     i=1
     while [[ -e ${filename}".a3m" ]]; do
         i=$((i+1))
     done
+
     if [[ $i -eq 1 ]]; then
         mv $file ${filename}".a3m"
     else
@@ -86,4 +97,3 @@ done
 # write log to file
 echo "[$(date '+%F %T')] ...done!" >> $PROGRESSLOG 
 echo End job: $(date)
-
